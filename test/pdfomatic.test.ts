1ac188f883a3f44eddc14bdec8d3a5886a8b566e
Jake Hendy <me@jakehendy.com>
2022-03-30 20:56:54 +0100

    Initial commit

---
+++ test/pdfomatic.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Pdfomatic from '../lib/pdfomatic-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pdfomatic-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Pdfomatic.PdfomaticStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
