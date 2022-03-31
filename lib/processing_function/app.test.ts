633d79b07935519f7d5e6f43d55d97495c4450c7
Jake Hendy <me@jakehendy.com>
2022-04-01 00:09:38 +0100

    Use SQSEvent and deserialize S3Event

--- lib/processing_function/app.test.ts
+++ lib/processing_function/app.test.ts
@@ -1,7 +1,15 @@
-import { S3EventRecord } from "aws-lambda"
-import { createHigherPrefix } from "./app"
+import { S3EventRecord, SQSEvent, SQSRecord } from "aws-lambda"
+import { createHigherPrefix, handler } from "./app"
+import { ListObjectsV2Command, ListObjectsV2CommandInput, ListObjectsV2CommandOutput, S3Client, _Object } from "@aws-sdk/client-s3";
+import { mockClient } from "aws-sdk-client-mock";
 
-require ('./app')
+
+
+const s3ClientMock = mockClient(S3Client);
+
+beforeAll(() => {
+    s3ClientMock.reset();
+})
 
 const createS3RecordForPath = (input: string): S3EventRecord => {
     return {
@@ -14,6 +22,18 @@ const createS3RecordForPath = (input: string): S3EventRecord => {
             }
         }
     } as S3EventRecord;
+}
+
+const createSqsEventForS3Events = (input: S3EventRecord[]): SQSEvent => {
+    const records: SQSRecord[] = [];
+    input.forEach(r => {
+        records.push({
+            body: JSON.stringify(r)
+        } as SQSRecord)
+    })
+    return {
+        Records: records
+    } as SQSEvent
 }
 
 test('Works on root object', () => {
@@ -35,4 +55,24 @@ test('Works on two folder in prefix', () => {
     const expected = "potato/pizza"
     const actual = createHigherPrefix(createS3RecordForPath(input));
     expect(actual).toBe(expected)
+})
+
+test('Handles SQSEvent', async () => {
+    s3ClientMock.on(ListObjectsV2Command).resolves(
+        {
+            Name: "pdfomatic.test",
+            KeyCount: 5,
+            Contents: [
+                {
+                    Key: "foo.pdf"
+                }
+            ] as _Object[]
+        } as ListObjectsV2CommandOutput
+    )
+    const input = createSqsEventForS3Events([createS3RecordForPath('potato/foo.pdf')]);
+    await handler(input, new Map<String, String>())
+    expect(s3ClientMock.call(0).args[0].input).toStrictEqual({
+        Bucket: "pdfomatic.test",
+        Prefix: "potato"
+    } as ListObjectsV2CommandInput)
 })
--- lib/processing_function/app.ts
+++ lib/processing_function/app.ts
@@ -8,12 +8,18 @@ export const createHigherPrefix = (record: S3EventRecord): string => {
     return key.join('/')
 }
 
-exports.handler = async (event: S3Event, _context: Map<String, String>): Promise<String> => {
+export const handler = async (event: SQSEvent, _context: Map<String, String>): Promise<String> => {
+    console.log(event);
     const client = new S3({region:event.Records[0].awsRegion});
-    const objects = await client.listObjectsV2({
-        Bucket: event.Records[0].s3.bucket.name,
-        Prefix: createHigherPrefix(event.Records[0])
-    })
-    console.log(`There are ${objects.KeyCount} objects in this prefix`)
+    for (let i = 0; i < event.Records.length; i++) { 
+        const record = event.Records[i];
+
+        const s3Event = JSON.parse(record.body) as S3EventRecord;
+        const objects = await client.listObjectsV2({
+            Bucket: s3Event.s3.bucket.name,
+            Prefix: createHigherPrefix(s3Event)
+        })
+        console.log(`There are ${objects.KeyCount} objects in this prefix`)
+    }
     return 'Hello World!';
 }
